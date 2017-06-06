663b2f6ad1e9442515acfaae4fb0a8f78c77669a
LinhBM <linhbm@outlook.com>
2017-06-06 15:13:12 +0700

    checkout 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :stripeToken
+
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
@@ -32,6 +34,11 @@ class User < ApplicationRecord
 
   scope :not_is_admin, (->_user{where admin: false})
 
+  enum role: [:user, :vip, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+  before_create :pay_with_card, unless: Proc.new { |user| user.admin? }
+  after_create :sign_up_for_mailing_list
+
   def follow other_user
     following << other_user
   end
@@ -58,5 +65,51 @@ class User < ApplicationRecord
 
   def liking? comment_id
     likeships.find_by comment_product_id: comment_id
+  end
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  def pay_with_card
+    if self.stripeToken.nil?
+      self.errors[:base] << t ".not_verify"
+      raise ActiveRecord::RecordInvalid.new(self)
+    end
+    customer = Stripe::Customer.create(
+      :email => self.email,
+      :card  => self.stripeToken
+    )
+    price = Rails.application.secrets.product_price
+    title = Rails.application.secrets.product_title
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => "#{price}",
+      :description => "#{title}",
+      :currency    => t ".usd"
+    )
+    Rails.logger.info(t ".transaction", self_email: self.email)
+      if charge[:paid] == true
+  rescue Stripe::InvalidRequestError => e
+    self.errors[:base] << e.message
+    raise ActiveRecord::RecordInvalid.new(self)
+  rescue Stripe::CardError => e
+    self.errors[:base] << e.message
+    raise ActiveRecord::RecordInvalid.new(self)
+  end
+
+  def sign_up_for_mailing_list
+    MailingListSignupJob.perform_later(self)
+  end
+
+  def subscribe
+    mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
+    list_id = Rails.application.secrets.mailchimp_list_id
+    result = mailchimp.lists(list_id).members.create(
+      body: {
+        email_address: self.email,
+        status: "subscribed"
+    })
+    Rails.logger.info(t ".subscribed", self_email: self.email) if result
   end
 end
