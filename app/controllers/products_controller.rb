654a70befbacacffd8592ffa55f5ff23b93c270a
LinhBM <linhbm@outlook.com>
2017-06-08 09:42:17 +0700

    ha

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,8 @@ class ProductsController < ApplicationController
 
   def index
     @products = @user.products
+    @search = Product.search(params[:q])
+    @result = @search.result
   end
 
   def new
